4635fd5983cd079949a6006e1a91ebefae20f839
lester88a <lester88a@gmail.com>
2015-03-10 12:45:25 -0400

    basic

---
+++ COMP123-Lesson9-Part1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP123_Lesson9_Part1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+
+
+        // UTILITY METHODS
+        private static void WaitForKey()
+        {
+            Console.WriteLine();
+            Console.WriteLine("++++++++++++++++++++++++++++++++++");
+            Console.WriteLine("Press any key to continue...");
+            Console.WriteLine("++++++++++++++++++++++++++++++++++");
+            Console.ReadKey();
+            Console.Clear();
+        }
+    }
+}
